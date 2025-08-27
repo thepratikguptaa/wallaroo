c0ee17d7ff3ded4b4d7b88c96a967c453457665d
Pratik Gupta <pratikguptamain@gmail.com>
2025-08-27 22:28:22 +0530

    add ImageKit authentication route

---
+++ app/api/imagekit-auth/route.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import ImageKit from "imagekit";
+import { NextResponse } from "next/server";
+
+const imagekit = new ImageKit({
+    publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+    privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+    urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+});
+
+export async function GET() {
+    try {
+        const authenticationParameters = imagekit.getAuthenticationParameters();
+        return NextResponse.json(authenticationParameters);
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
