8d34e28126df571b47d9eb2896d175fecde69587
Pratik Gupta <pratikguptamain@gmail.com>
2025-08-28 22:59:16 +0530

    add Providers component with ImageKit and Notification context

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -4,8 +4,8 @@ import { SessionProvider } from "next-auth/react";
 import { ImageKitProvider } from "imagekitio-next";
 import { NotificationProvider } from "./Notification";
 
-const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
-const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY!;
+const urlEndpoint = process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!;
+const publicKey = process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!;
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     const authenticator = async () => {
